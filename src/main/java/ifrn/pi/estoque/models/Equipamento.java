bbf3d08cf490d22bd6c87cc9f7339e39aa2f4069
Brenda Michelle <brendamichelle013@gmail.com>
2021-03-19 03:51:02 -0300

    Criando modelo para Estoque

---
+++ src/main/java/ifrn/pi/estoque/models/Equipamento.java
@@ -0,0 +1,33 @@
+package ifrn.pi.estoque.models;
+
+public class Equipamento {
+
+	private String data;
+	private String nome;
+	private String quantidade;
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(String quantidade) {
+		this.quantidade = quantidade;
+	}
+
+}
