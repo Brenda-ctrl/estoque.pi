198be3affae3b5c19fa708ffe926c0bf349481ac
Brenda Michelle <brendamichelle013@gmail.com>
2021-03-19 17:46:40 -0300

    Configurando classe de modelo Equipamento

--- src/main/java/ifrn/pi/estoque/models/Equipamento.java
+++ src/main/java/ifrn/pi/estoque/models/Equipamento.java
@@ -1,10 +1,29 @@
 package ifrn.pi.estoque.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Equipamento {
 
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY )
+	
+	private Long id;
 	private String data;
 	private String nome;
 	private String quantidade;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
 
 	public String getData() {
 		return data;
